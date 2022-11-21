3328961b6ab44d531652fcdfab12ac72294e2345
Ashish Kumar <akumar@ampliforce.com>
2022-11-21 18:44:05 +0530

    restructure project and multer install

---
+++ controller/products/productsController.js
@@ -0,0 +1,37 @@
+const productModal = require("../../model/products/products");
+
+module.exports = {
+  createProduct: async (req, res) => {
+    try {
+      //   let data = req.body;
+      //   let saveData = await new productModal(req.body).save();
+      let saveData = await new productModal({
+        title: req.body.title,
+        price: req.body.price,
+        category: req.body.category,
+        description: req.body.description,
+        image: req.body.image,
+      }).save();
+      if (!saveData) {
+        return res.send({
+          status: false,
+          message: "Internal Server Error.",
+          result: [],
+        });
+      } else {
+        return res.send({
+          starus: true,
+          message: "Product created Successfully.",
+          result: { saveData },
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      return res.send({
+        status: false,
+        message: "Internal Server Error.",
+        result: [],
+      });
+    }
+  },
+};
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,31 +1,32 @@
-const jwt = require('jsonwebtoken');
-const userModel = require('../model/userModel')
+const jwt = require("jsonwebtoken");
+const userModel = require("../model/core/userModel");
 
 module.exports = {
-    jwtToken: async (req, res, next) => {
-        try {
-            let decode = await jwt.verify(req.headers.token, 'geeksshop_db');
-            if (decode) {
-                let userData = await userModel.findOne({ _id: decode.userId, userType: 'USER' });
-                if (userData) {
-                    if (userData.status == 'BLOCK') {
-                        res.send({ responseMessage: 'BLOCK' })
-                    }
-                    else if (userData.status == 'DELETE') {
-                        res.send({ responseMessage: 'DELETE' })
-                    }
-                    else {
-                        req.userId = userData._id;
-                        next()
-                    }
-                }
-            }
-        } catch (error) {
-            return res.send({
-                responseCode: 501,
-                responseMessage: "Something went wrong!",
-                responseResult: error.message,
-            });
+  jwtToken: async (req, res, next) => {
+    try {
+      let decode = await jwt.verify(req.headers.token, "geeksshop_db");
+      if (decode) {
+        let userData = await userModel.findOne({
+          _id: decode.userId,
+          userType: "USER",
+        });
+        if (userData) {
+          if (userData.status == "BLOCK") {
+            res.send({ responseMessage: "BLOCK" });
+          } else if (userData.status == "DELETE") {
+            res.send({ responseMessage: "DELETE" });
+          } else {
+            req.userId = userData._id;
+            next();
+          }
         }
+      }
+    } catch (error) {
+      return res.send({
+        responseCode: 501,
+        responseMessage: "Something went wrong!",
+        responseResult: error.message,
+      });
     }
+  },
 };
