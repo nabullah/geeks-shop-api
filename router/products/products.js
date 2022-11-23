192a4e602ee6c5a1e487ed5bd6ceeb5d6bc3b47f
Ashish Kumar <akumar@ampliforce.com>
2022-11-23 17:14:59 +0530

    changes

--- router/products/products.js
+++ router/products/products.js
@@ -3,6 +3,5 @@ const productsController = require("../../controller/products/productsController
 const auth = require("../../middleware/auth");
 
 productsRouter.post("/create", productsController.createProduct);
-// userRouter.post("/login", userController.login);
 
 module.exports = productsRouter;
